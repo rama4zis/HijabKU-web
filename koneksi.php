25c5bba3f84e528200fa0f5ee67606eb4d153445
rama <rama.4ditya3@gmail.com>
2025-06-29 04:14:05 +0700

    update database connection credentials in koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "hijabku_db");
+$koneksi = mysqli_connect("localhost", "hijab", "hijab123", "hijabku_db");
 // Check connection
 if (mysqli_connect_errno()) {
 	echo "Koneksi database gagal : " . mysqli_connect_error();
